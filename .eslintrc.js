dc006ea3901b4a21124a3711fb906b1ece2ee1fb
spectrallll <tsska_tsska@mail.ru>
2022-10-08 21:10:30 +0300

    add stylelint, fix scss files, add i18next eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     "plugin:react/recommended",
     "airbnb",
+    "plugin:i18next/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -18,6 +19,7 @@ module.exports = {
   plugins: [
     "react",
     "@typescript-eslint",
+    "i18next",
   ],
   rules: {
     "react/jsx-indent": [2, 2],
@@ -36,6 +38,7 @@ module.exports = {
     "import/extensions": "off",
     "import/no-extraneous-dependencies": "warn",
     "no-underscore-dangle": "off",
+    "i18next/no-literal-string": ["error", { markupOnly: true }],
   },
   globals: {
     __IS_DEV__: true,

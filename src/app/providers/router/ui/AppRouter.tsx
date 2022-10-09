69ca721b9725c02926b803fd0f1d636863da347f
spectrallll <tsska_tsska@mail.ru>
2022-10-09 21:43:37 +0300

    add unexpected route, add NotFoundPage, add Loader component, add PageLoader component, fix styles

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { PageLoader } from "widgets/PageLoader/ui/PageLoader";
 
 const AppRouter = () => (
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<PageLoader />}>
     <Routes>
       {Object.values(routeConfig).map(({ element, path }) => (
         <Route

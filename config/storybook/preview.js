bf2eb7a7bc88015f207db2c8d3a56339771cfb05
spectrallll <tsska_tsska@mail.ru>
2022-10-16 18:18:41 +0300

    fix storybook cfg

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,6 +3,7 @@ import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "../../src/app/providers/ThemeProvider";
 import { RouteDecorator } from "../../src/shared/config/storybook/RouteDecorator/RouteDecorator";
+import "loki/configure-react";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },

37f48cc1377c4d9759713ca9c89b4331867d7f80
spectrallll <tsska_tsska@mail.ru>
2022-10-25 16:54:29 +0300

    add storybook for LoginForm, update screenshots

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import { Theme } from "app/providers/ThemeProvider";
+import { LoginForm } from "./LoginForm";
+
+export default {
+  title: "features/LoginForm",
+  component: LoginForm,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof LoginForm>;
+
+const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {};
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];

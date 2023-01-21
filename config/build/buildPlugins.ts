e6a81c730a1f683a0f35b194d241422ee974e0a8
spectrallll <tsska_tsska@mail.ru>
2023-01-21 14:45:52 +0300

    fix api config, add locales to build, edit webpack config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import CopyPlugin from "copy-webpack-plugin";
 import { BuildOptions } from "./types/config";
 
 export function buildPlugins({
@@ -21,6 +22,13 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: paths.locales, to: paths.buildLocales,
+        },
+      ],
     }),
   ];
 
--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -4,7 +4,9 @@ export interface BuildPaths {
     entry: string;
     build: string;
     html: string;
-    src: string
+    src: string;
+    locales: string; // Путь до файлов с переводами
+    buildLocales: string; // Куда переводы перемещать
 }
 
 export interface BuildEnv {

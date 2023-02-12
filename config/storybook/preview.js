3b45840ca07303f50460f3e856eda63ab10d516a
spectrallll <tsska_tsska@mail.ru>
2023-02-12 17:21:46 +0300

    update screenshots, fix jest cfg

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,7 +3,7 @@ import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { RouteDecorator } from "../../src/shared/config/storybook/RouteDecorator/RouteDecorator";
 import { SuspenseDecorator } from "../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator";
-import { StoreDecorator } from "../../src/shared/config/storybook/StoreDecorator/StoreDecorator";
+import { Theme } from "../../src/shared/const/theme";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -13,6 +13,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  layout: "fullscreen",
 };
 
 addDecorator(StyleDecorator);

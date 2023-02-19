ed0a781f2001274aed7990f6d05faa4bbfdc85e7
spectrallll <tsska_tsska@mail.ru>
2023-02-19 15:22:29 +0300

    fix build error

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.stories.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.stories.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import withMock from "storybook-addon-mock";
 import { ThemeDecorator } from "@/shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { NotificationButton } from "./NotificationButton";
 import { StoreDecorator } from "@/shared/config/storybook/StoreDecorator/StoreDecorator";
@@ -12,7 +11,6 @@ export default {
   argTypes: {
     backgroundColor: { control: "color" },
   },
-  decorators: [withMock],
 } as ComponentMeta<typeof NotificationButton>;
 
 const Template: ComponentStory<typeof NotificationButton> = (args) => <NotificationButton {...args} />;
